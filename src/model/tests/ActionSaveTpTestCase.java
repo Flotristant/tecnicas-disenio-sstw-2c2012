ef76badc5b0f07a7bfa8210141b7e351cf761b9a
jose.francisco.tw@gmail.com <jose.francisco.tw@gmail.com@3e20bac7-f3f0-c661-3f92-5f983b4c4eb0>
2012-11-07 19:52:27 +0000

    finished various Actions

--- src/model/tests/ActionSaveTpTestCase.java
+++ src/model/tests/ActionSaveTpTestCase.java
@@ -1,6 +1,5 @@
 package model.tests;
 
-
 import java.util.HashMap;
 
 import junit.framework.Assert;
@@ -10,6 +9,7 @@ import model.Message;
 import model.Rule;
 import model.RuleAltaMateria;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import persistence.mocks.TpPersistenceMock;
@@ -17,25 +17,34 @@ import persistence.mocks.TpPersistenceMock;
 
 public class ActionSaveTpTestCase {
 
+	private Rule rule;
+	private ActionSaveTp saveTp;
+	private TpPersistenceMock tpPersistence;
+
+	@Before
+	public void setUp() throws Exception {
+		this.tpPersistence = new TpPersistenceMock();
+		this.saveTp = new ActionSaveTp(this.tpPersistence);
+	}
+	
 	@Test
 	public void testShouldPassWhenSaveTp() {
-		Rule rule = new RuleAltaMateria("ruleAltaMateria");
-		rule.setCodigoMateria("75.05");
-		rule.setTpNumber("8");
-		TpPersistenceMock tpPersistence = new TpPersistenceMock();
-		Message message = new Message("sender", "", "", "");
+		this.rule = new RuleAltaMateria("ruleAltaMateria");
+		this.rule.setCodigoMateria("75.05");
+		this.rule.setTpNumber("8");
+		
+		Message message = new Message("sender", "to", "subject", "body");
 		message.addAttachment("key1", "88".getBytes());
 		message.addAttachment("key2", "89".getBytes());
 		
-		ActionSaveTp saveTp = new ActionSaveTp(tpPersistence);
-		saveTp.initialize(rule, message);
-		saveTp.execute();
+		this.saveTp.initialize(this.rule, message);
+		this.saveTp.execute();
 		
-		Assert.assertEquals("75.05", tpPersistence.getCodigoMateriaToSave());
-		Assert.assertEquals("sender", tpPersistence.getSenderToSave());
-		Assert.assertEquals(Integer.valueOf(8), tpPersistence.getTpNumberToSave());
+		Assert.assertEquals("75.05", this.tpPersistence.getCodigoMateriaToSave());
+		Assert.assertEquals("sender", this.tpPersistence.getSenderToSave());
+		Assert.assertEquals(Integer.valueOf(8), this.tpPersistence.getTpNumberToSave());
 		
-		HashMap<String,byte[]> attachToSave = (HashMap<String, byte[]>) tpPersistence.getAttachmentsToSave();
+		HashMap<String,byte[]> attachToSave = (HashMap<String, byte[]>) this.tpPersistence.getAttachmentsToSave();
 		
 		Assert.assertNotNull(attachToSave.get("key1"));
 		Assert.assertNotNull(attachToSave.get("key2"));
@@ -43,5 +52,9 @@ public class ActionSaveTpTestCase {
 		Assert.assertEquals(message.getAttachments().get("key1"), attachToSave.get("key1"));
 		Assert.assertEquals(message.getAttachments().get("key2"), attachToSave.get("key2"));
 		Assert.assertNull(attachToSave.get("key3"));
+		
+		Assert.assertEquals(Integer.valueOf(this.rule.getTpNumber()), this.tpPersistence.getTpNumberToSave());
+		Assert.assertEquals(this.rule.getCodigoMateria(), this.tpPersistence.getCodigoMateriaToSave());
+		Assert.assertEquals(message.getSender(), this.tpPersistence.getSenderToSave());
 	}
 }
---
+++ src/model/tests/ActionValidateSenderTestCase.java
@@ -0,0 +1,40 @@
+package model.tests;
+
+import model.ActionValidateSender;
+import model.Message;
+import model.RuleAltaGrupo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import persistence.mocks.MailPersistenceMock;
+
+public class ActionValidateSenderTestCase {
+
+	private ActionValidateSender validatorEmail;
+	private MailPersistenceMock mailPersistence;
+
+	@Before
+	public void setUp() throws Exception {
+		this.mailPersistence = new MailPersistenceMock();
+		this.validatorEmail = new ActionValidateSender(mailPersistence);
+	}
+	
+	@Test
+	public void testShouldReturnTrueWhenValidateEmail() {
+		this.validatorEmail.initialize(new RuleAltaGrupo("rule1") , new Message("francisco", "", "subject", "body"));
+		
+		Assert.assertTrue(this.validatorEmail.execute());
+		
+		Assert.assertEquals("francisco", this.mailPersistence.getDirSender());
+	}
+	
+	@Test
+	public void testShouldFailIfMailIsNotInDatabase() {
+		this.validatorEmail.initialize(new RuleAltaGrupo("rule1") , new Message("caty", "", "subject", "body"));
+		
+		Assert.assertFalse(this.validatorEmail.execute());
+	}
+	
+}
