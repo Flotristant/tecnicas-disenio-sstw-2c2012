9142cf5382e193d5a7a14cd9174ac7c21161ce66
jose.francisco.tw@gmail.com <jose.francisco.tw@gmail.com@3e20bac7-f3f0-c661-3f92-5f983b4c4eb0>
2012-11-15 20:17:39 +0000

    implemented DBPersistence and refactor TableGroupAlumno

--- trunk/src/persistence/tests/DBTpPersistenceTestCase.java
+++ trunk/src/persistence/tests/DBTpPersistenceTestCase.java
@@ -26,11 +26,17 @@ public class DBTpPersistenceTestCase {
 		Class.forName("org.sqlite.JDBC");
 		this.conn = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db", this.codigoMateria)); 
         this.statement = this.conn.createStatement();
-        this.statement.executeUpdate("CREATE TABLE IF NOT EXISTS ALUMNO (Padron int PRIMARY KEY, Sender text, GroupNr int);");
-        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender, GroupNr) VALUES (%d, '%s', %d);", 90117, this.sender, null));
-        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender, GroupNr) VALUES (%d, '%s', %d);", 91227, "francisco", 1));
-        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender, GroupNr) VALUES (%d, '%s', %d);", 91678, "caty@Hola1", 1));
-        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender, GroupNr) VALUES (%d, '%s', %d);", 91000, "alguien", 1));
+        
+        this.statement.executeUpdate("CREATE TABLE IF NOT EXISTS ALUMNO (Padron int PRIMARY KEY, Sender text);");
+        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender) VALUES (%d, '%s');", 90117, this.sender));
+        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender) VALUES (%d, '%s');", 91227, "francisco"));
+        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender) VALUES (%d, '%s');", 91678, "caty@Hola1"));
+        this.statement.executeUpdate(String.format("INSERT INTO ALUMNO (Padron, Sender) VALUES (%d, '%s');", 91000, "alguien"));
+        
+        this.statement.executeUpdate("CREATE TABLE IF NOT EXISTS GROUPALUMNO (Padron int, GroupNr int, PRIMARY KEY(Padron, GroupNr), FOREIGN KEY(Padron) REFERENCES ALUMNO(Padron));");
+        this.statement.executeUpdate(String.format("INSERT INTO GROUPALUMNO (Padron, GroupNr) VALUES (%d, %d);", 91227, 1));
+        this.statement.executeUpdate(String.format("INSERT INTO GROUPALUMNO (Padron, GroupNr) VALUES (%d, %d);", 91678, 1));
+        this.statement.executeUpdate(String.format("INSERT INTO GROUPALUMNO (Padron, GroupNr) VALUES (%d, %d);", 91000, 1));
 	}
 	
 	@Test
@@ -41,6 +47,21 @@ public class DBTpPersistenceTestCase {
 		
 		Assert.assertTrue(db.isTPDelivered(this.codigoMateria, this.sender, 1));
 		Assert.assertFalse(db.isTPDelivered(this.codigoMateria, "20", 30));
+	}
+	
+	@Test
+	public void testShouldReturnTheStudentsThatHasntGroup() throws Exception{
+		DBTpPersistence db = new DBTpPersistence();
+		Iterable<Integer> it = db.getPadronesFromGroupOfTheSender(this.codigoMateria, this.sender);
+		
+		ArrayList<Integer> padrones = new ArrayList<Integer>();
+		
+		for (Integer padron : it)
+			padrones.add(padron);
+			
+		Assert.assertEquals(1, padrones.size());
+		
+		Assert.assertEquals("90117", padrones.get(0).toString());
 	}
 	
 	@Test
