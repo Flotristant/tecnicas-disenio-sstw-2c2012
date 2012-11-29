d8a9c029455b41466ae8b4afde7bc3d789e31921
aletorrado@gmail.com <aletorrado@gmail.com@3e20bac7-f3f0-c661-3f92-5f983b4c4eb0>
2012-11-29 17:59:51 +0000

    Cambio interfaz, Demonio del servicio hecho con Thread.

---
+++ trunk/src/application/Application.java
@@ -0,0 +1,71 @@
+package application;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import application.DaemonThread.StatusChangedListener;
+import application.DaemonThread.StatusChangedListener.status;
+
+import ui.*;
+
+public class Application {
+	
+	public static DaemonThread thread;
+	public static UI ui;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		ui = new UI(new UI.UIActionListener(){
+
+			@Override
+			public boolean onAddSubjectClicked(int subject, int student) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void onDeleteSubjectClicked(int subject, int student) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onRun() {
+				thread = new DaemonThread();
+				thread.onStatusChanged(new StatusChangedListener() {
+					
+					public void newStatus(status s) {
+						switch (s) {
+						case Up:
+							ui.setStatus(UI.Status.Up);
+							break;
+						case Error:
+							ui.setStatus(UI.Status.Error);
+							break;
+						case Down:
+							ui.setStatus(UI.Status.Down);
+							break;
+						}
+					}
+				});
+				thread.start();
+			}
+
+			@Override
+			public void onStop() {
+				thread.die();
+				
+			}
+		});
+		ui.setVisible(true);
+	}
+
+}
